be48502eedc10bda0a253139e118648bfdbc9698
Danila Seleznev <danila.seleznev.03@mail.ru>
2025-01-23 16:46:43 +0400

    Feat: trial rendering for projectile

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+	static private Slingshot S;
 	[Header("set in inspector")]
 	public GameObject prefabProjectile;
 	public float velMult=8f;
@@ -15,8 +16,18 @@ public class Slingshot : MonoBehaviour
 	public bool aimingMode;
 	private Rigidbody _projectileRigidbody;
 	private float _maxMagnitude;
+	static public Vector3 LAUNCH_POS
+	{
+		get
+		{
+			if (S==null) return Vector3.zero;
+			return S.launchPos;
+		}
+		
+	}
 	void Awake()
 	{
+		S=this;
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
